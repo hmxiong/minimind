7d0cf63c097f9891440f08c6a10373e0218394e4
hmxiong <haomiaoxiong12138@gmail.com>
2026-04-29 19:57:57 +0800

    update_fsdp

--- train_8gpu.sh
+++ train_8gpu.sh
@@ -4,10 +4,10 @@ torchrun --standalone --nproc_per_node=8 train_pretrain_fsdp.py \
   --data_path ../dataset/pretrain_t2t_mini.jsonl \
   --dtype bfloat16 \
   --sharding full \
-  --hidden_size 2048 \
-  --num_hidden_layers 24 \
+  --hidden_size 1344 \
+  --num_hidden_layers 16 \
   --max_seq_len 1024 \
-  --batch_size 24 \
+  --batch_size 12 \
   --accumulation_steps 2 \
   --num_workers 12 \
   --save_weight pretrain_fsdp
